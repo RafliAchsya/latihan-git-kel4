e33db44d7b9a9a803886c610251d2fabebb12816
RafliAchsya <rapstronaut25@gmail.com>
2023-04-17 14:21:17 +0700

    update3

--- application/views/view-form-matakuliah.php
+++ application/views/view-form-matakuliah.php
@@ -6,7 +6,7 @@
 
 <body>
     <center>
-        <form action="<?= base_url('matakuliah/cetak'); ?>" method="post">
+        <form action="<?= base_url('index.php/Matakuliah/cetak'); ?>" method="post">
             <table>
                 <tr>
                     <th colspan="3">Form Input Data Mata Kuliah</th>
